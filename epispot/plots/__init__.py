f6830ece240d57943c4895604bafa3c7acc886b5
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2022-08-12 17:01:00 -0700

    Cut all code to 79 chars and disable E501
    E501 is reponsible for 225 errors (most of which occur with documentation strings and not actual code).
    Additionally, these errors are not automatically corrected for docstrings by `autopep8`, so we have no choice but to disable this error for the sake of saving time.
    Code can be maintained to be well under the 79 character limit, which is  what has been done before the rule was disabled.

--- epispot/plots/__init__.py
+++ epispot/plots/__init__.py
@@ -10,7 +10,10 @@ def dependency_check():
     try:
         import plotly
     except ImportError:  # pragma: no cover
-        raise ImportError('In order to create interactive web-based plots, it is highly recommended that you install `plotly` as an experimental dependency.\n'
+        raise ImportError('In order to create interactive web-based '
+                          'plots, it is highly recommended that you '
+                          'install `plotly` as an experimental '
+                          'dependency.\n'
                           'Install with either:\n'
                           ' $ pip install plotly\n'
                           ' $ conda install -c conda-forge plotly')
@@ -18,9 +21,15 @@ def dependency_check():
         from matplotlib import pyplot as plt
         plt.style.use('science')  # `SciencePlots` cannot be imported directly
     except ImportError:  # pragma: no cover
-        raise ImportError('In order to create scientific plots with `matplotlib`, it is highly recommended that you install `SciencePlots` as an experimental'
-                          'dependency. Please note that `SciencePlots` is only available via `pip` at this time. If using Anaconda, '
-                          'use the pre-existing `pip` installation to add `SciencePlots` to your environment.\n'
+        raise ImportError('In order to create scientific plots with '
+                          '`matplotlib`, it is highly recommended that '
+                          'you install `SciencePlots` as an '
+                          'experimental dependency. '
+                          'Please note that `SciencePlots` is only '
+                          'available via `pip` at this time. '
+                          'If using Anaconda, use the pre-existing '
+                          '`pip` installation to add `SciencePlots` to '
+                          'your environment.\n'
                           'Install with:\n'
                           ' $ pip install SciencePlots')
 
