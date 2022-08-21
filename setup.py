2bfdf7b794cbf6774e0f6acd6437c8916bf1caea
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2022-08-20 18:44:08 -0700

    Remove incorrect Python 3.7 tag from setup files
    Python 3.7 was dropped from epispot's supported languages after it was officially end-of-lifed

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setuptools.setup(
     },
     packages=setuptools.find_packages('epispot/'),
     classifiers=[
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
